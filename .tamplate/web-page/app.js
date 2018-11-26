af3bdeb471063765d09766015da706693d53a339
wen911119 <wen911119@gmail.com>
2018-11-26 19:13:47 +0800

    stash

---
+++ .tamplate/web-page/app.js
@@ -0,0 +1,12 @@
+import { h, Component } from "preact";
+
+@WithNav
+export default class ###_page-name_### extends Component {
+  render() {
+    return (
+      <div>
+        ###_page-name_###
+      </div>
+    );
+  }
+}
