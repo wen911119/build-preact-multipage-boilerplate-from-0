8d9e2903ad92446aefd1e90e340f9fee4082e0ca
wen911119 <wen911119@gmail.com>
2018-11-27 15:54:09 +0800

    done

--- updateWechatIP.js
+++ updateWechatIP.js
@@ -1,16 +1,12 @@
-const globby = require('globby');
 const ip = require("ip");
 const fse = require('fs-extra');
+const currentDir = process.cwd();
+const argvs = process.argv.slice(2)
+const port = argvs[0] || '8081';
+
 (async ()=>{
-  const paths = await globby('wechat/pages/*/*.wxml')
-  
-  const rets = await Promise.all(paths.map(async path=>{
-    let content = await fse.readFile(path, 'utf8')
-    content = content.replace(/\d+\.\d+\.\d+\.\d+/, ip.address())
-    const ret = await fse.writeFile(path, content, 'utf8')
-    return ret
-  }))
-  if (rets.some(ret=>!!ret)) {
-    console.log('更新ip失败！')
-  }
+  const targetPath = `${currentDir}/wechat/app.js`
+  let content = await fse.readFile(targetPath, 'utf8')
+  content = content.replace(/\d+\.\d+\.\d+\.\d+:\d+/, `${ip.address()}:${port}`)
+  await fse.writeFile(targetPath, content, 'utf8')
 })()
