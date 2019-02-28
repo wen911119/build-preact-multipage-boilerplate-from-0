d68aea8c14ee66f920ae004ad92dca1cf63fc7a4
wen911119 <wen911119@gmail.com>
2019-02-28 18:49:32 +0800

    feat: new page zfDetail

---
+++ src/pages/zfDetail/app.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact'
+
+export default class ZfDetailPage extends Component {
+  render () {
+    return (
+      <div>
+        ZfDetailPage
+      </div>
+    )
+  }
+}
